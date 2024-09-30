73ccd04a36a4729a97b8fe70be344b7042dc61c2
Abboskin <abboskinsenpai@gmail.com>
2024-09-30 20:02:43 +0500

    INSERT, UPDATE, DELETE, MERGE, TRANSACTION

---
+++ Section 11. Modifying data.sql
@@ -0,0 +1,519 @@
+--Section 11. Modifying data
+--SQL Server INSERT statement examples
+CREATE TABLE sales.promotions (
+    promotion_id INT PRIMARY KEY IDENTITY (1, 1),
+    promotion_name VARCHAR (255) NOT NULL,
+    discount NUMERIC (3, 2) DEFAULT 0,
+    start_date DATE NOT NULL,
+    expired_date DATE NOT NULL
+); 
+
+--1) Basic INSERT example
+INSERT INTO sales.promotions (
+    promotion_name,
+    discount,
+    start_date,
+    expired_date
+)
+VALUES
+    (
+        '2018 Summer Promotion',
+        0.15,
+        '20180601',
+        '20180901'
+    );
+
+SELECT * FROM sales.promotions;
+
+--2) Insert and return inserted values
+INSERT INTO sales.promotions (
+    promotion_name,
+    discount,
+    start_date,
+    expired_date
+) OUTPUT inserted.promotion_id
+VALUES
+    (
+        '2018 Fall Promotion',
+        0.15,
+        '20181001',
+        '20181101'
+    );
+
+
+
+
+insert into sales.promotions (
+	promotion_name,
+	discount,
+	start_date,
+	expired_date
+) Output inserted.promotion_id,
+inserted.promotion_name,
+inserted.discount,
+inserted.start_date,
+inserted.expired_date
+Values
+('2018 Winter Promotion',
+0.2,
+'20181201',
+'20190101'
+);
+
+--3) Insert explicit values into the identity column
+
+SET IDENTITY_INSERT sales.promotions ON;
+
+INSERT INTO sales.promotions (
+    promotion_id,
+    promotion_name,
+    discount,
+    start_date,
+    expired_date
+) OUTPUT inserted.promotion_id
+VALUES
+    (
+        4,
+        '2019 Spring Promotion',
+        0.25,
+        '20190201',
+        '20190301'
+    );
+
+SELECT * FROM sales.promotions;
+
+--SQL Server INSERT multiple rows – examples
+--1) Inserting multiple rows example
+SET IDENTITY_INSERT sales.promotions OFF;
+INSERT INTO sales.promotions (
+    promotion_name,
+    discount,
+    start_date,
+    expired_date
+)
+VALUES
+    (
+        '2019 Summer Promotion',
+        0.15,
+        '20190601',
+        '20190901'
+    ),
+    (
+        '2019 Fall Promotion',
+        0.20,
+        '20191001',
+        '20191101'
+    ),
+    (
+        '2019 Winter Promotion',
+        0.25,
+        '20191201',
+        '20200101'
+    );
+select * from sales.promotions 
+
+--2) Inserting multiple rows and returning the inserted id list example
+INSERT INTO 
+	sales.promotions ( 
+		promotion_name, discount, start_date, expired_date
+	)
+OUTPUT inserted.promotion_id
+VALUES
+	('2020 Summer Promotion',0.25,'20200601','20200901'),
+	('2020 Fall Promotion',0.10,'20201001','20201101'),
+	('2020 Winter Promotion', 0.25,'20201201','20210101');
+
+--Introduction to SQL Server INSERT INTO SELECT statement
+CREATE TABLE sales.addresses (
+    address_id INT IDENTITY PRIMARY KEY,
+    street VARCHAR (255) NOT NULL,
+    city VARCHAR (50),
+    state VARCHAR (25),
+    zip_code VARCHAR (5)
+);   
+
+--1) Insert all rows from another table example
+INSERT INTO sales.addresses (street, city, state, zip_code) 
+SELECT
+    street,
+    city,
+    state,
+    zip_code
+FROM
+    sales.customers
+ORDER BY
+    first_name,
+    last_name; 
+
+select * from sales.addresses
+
+--2) Insert some rows from another table example
+INSERT INTO 
+    sales.addresses (street, city, state, zip_code) 
+SELECT
+    street,
+    city,
+    state,
+    zip_code
+FROM
+    sales.stores
+WHERE
+    city IN ('Santa Cruz', 'Baldwin')
+
+--3) Insert the top N of rows
+TRUNCATE TABLE sales.addresses;
+
+INSERT TOP (10) 
+INTO sales.addresses (street, city, state, zip_code) 
+SELECT
+    street,
+    city,
+    state,
+    zip_code
+FROM
+    sales.customers
+ORDER BY
+    first_name,
+    last_name;
+
+--4) Insert the top percent of rows
+TRUNCATE TABLE sales.addresses;
+
+INSERT TOP (10) PERCENT  
+INTO sales.addresses (street, city, state, zip_code) 
+SELECT
+    street,
+    city,
+    state,
+    zip_code
+FROM
+    sales.customers
+ORDER BY
+    first_name,
+    last_name;
+---------------------------------------------
+--SQL Server UPDATE Statement
+CREATE TABLE sales.taxes (
+	tax_id INT PRIMARY KEY IDENTITY (1, 1),
+	state VARCHAR (50) NOT NULL UNIQUE,
+	state_tax_rate DEC (3, 2),
+	avg_local_tax_rate DEC (3, 2),
+	combined_rate AS state_tax_rate + avg_local_tax_rate,
+	max_local_tax_rate DEC (3, 2),
+	updated_at datetime
+);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Alabama',0.04,0.05,0.07);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Alaska',0,0.01,0.07);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Arizona',0.05,0.02,0.05);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Arkansas',0.06,0.02,0.05);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('California',0.07,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Colorado',0.02,0.04,0.08);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Connecticut',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Delaware',0,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Florida',0.06,0,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Georgia',0.04,0.03,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Hawaii',0.04,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Idaho',0.06,0,0.03);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Illinois',0.06,0.02,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Indiana',0.07,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Iowa',0.06,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Kansas',0.06,0.02,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Kentucky',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Louisiana',0.05,0.04,0.07);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Maine',0.05,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Maryland',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Massachusetts',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Michigan',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Minnesota',0.06,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Mississippi',0.07,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Missouri',0.04,0.03,0.05);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Montana',0,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Nebraska',0.05,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Nevada',0.06,0.01,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('New Hampshire',0,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('New Jersey',0.06,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('New Mexico',0.05,0.02,0.03);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('New York',0.04,0.04,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('North Carolina',0.04,0.02,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('North Dakota',0.05,0.01,0.03);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Ohio',0.05,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Oklahoma',0.04,0.04,0.06);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Oregon',0,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Pennsylvania',0.06,0,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Rhode Island',0.07,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('South Carolina',0.06,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('South Dakota',0.04,0.01,0.04);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Tennessee',0.07,0.02,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Texas',0.06,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Utah',0.05,0,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Vermont',0.06,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Virginia',0.05,0,0);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Washington',0.06,0.02,0.03);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('West Virginia',0.06,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Wisconsin',0.05,0,0.01);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('Wyoming',0.04,0.01,0.02);
+INSERT INTO sales.taxes(state,state_tax_rate,avg_local_tax_rate,max_local_tax_rate) VALUES('D.C.',0.05,0,0);
+
+
+
+--1) Update a single column in all rows of a table
+UPDATE sales.taxes
+SET updated_at = GETDATE();
+
+SELECT * FROM sales.taxes;
+
+UPDATE sales.taxes
+SET max_local_tax_rate += 0.02,
+    avg_local_tax_rate += 0.01
+WHERE
+    max_local_tax_rate = 0.01;
+
+--SQL Server UPDATE JOIN
+DROP TABLE IF EXISTS sales.targets;
+
+CREATE TABLE sales.targets
+(
+    target_id  INT	PRIMARY KEY, 
+    percentage DECIMAL(4, 2) 
+        NOT NULL DEFAULT 0
+);
+
+INSERT INTO 
+    sales.targets(target_id, percentage)
+VALUES
+    (1,0.2),
+    (2,0.3),
+    (3,0.5),
+    (4,0.6),
+    (5,0.8);
+
+CREATE TABLE sales.commissions
+(
+    staff_id    INT PRIMARY KEY, 
+    target_id   INT, 
+    base_amount DECIMAL(10, 2) 
+        NOT NULL DEFAULT 0, 
+    commission  DECIMAL(10, 2) 
+        NOT NULL DEFAULT 0, 
+    FOREIGN KEY(target_id) 
+        REFERENCES sales.targets(target_id), 
+    FOREIGN KEY(staff_id) 
+        REFERENCES sales.staffs(staff_id),
+);
+
+INSERT INTO 
+    sales.commissions(staff_id, base_amount, target_id)
+VALUES
+    (1,100000,2),
+    (2,120000,1),
+    (3,80000,3),
+    (4,900000,4),
+    (5,950000,5);
+
+select * from sales.targets
+select * from sales.commissions
+
+UPDATE
+    sales.commissions
+SET
+    sales.commissions.commission = 
+        c.base_amount * t.percentage
+FROM 
+    sales.commissions c
+    INNER JOIN sales.targets t
+        ON c.target_id = t.target_id;
+
+SELECT * FROM sales.commissions;
+
+--B) SQL Server UPDATE LEFT JOIN example
+INSERT INTO 
+    sales.commissions(staff_id, base_amount, target_id)
+VALUES
+    (6,100000,NULL),
+    (7,120000,NULL);
+
+UPDATE 
+    sales.commissions
+SET  
+    sales.commissions.commission = 
+        c.base_amount  * COALESCE(t.percentage,0.1)
+FROM  
+    sales.commissions c
+    LEFT JOIN sales.targets t 
+        ON c.target_id = t.target_id;
+
+-------------------------------------------------------------------
+--SQL Server DELETE
+SELECT * 
+INTO production.product_history
+FROM
+    production.products;
+
+SELECT * FROM production.product_history;
+
+--1) Delete the number of random rows example
+DELETE TOP (21)
+FROM production.product_history;
+
+--2) Delete the percent of random rows example
+DELETE TOP (5) PERCENT
+FROM production.product_history;
+
+--3) Delete some rows with a condition example
+DELETE
+FROM
+    production.product_history
+WHERE
+    model_year = 2017;
+
+--4) Delete all rows from a table example
+DELETE FROM  production.product_history;
+
+SELECT * FROM production.product_history;
+---------------------------------------------------------------
+
+--Introduction SQL Server MERGE Statement
+--If you use the INSERT, UPDATE, and DELETE statement individually,
+--you have to construct three separate statements to update the data
+--to the target table with the matching rows from the source table.
+--However, SQL Server provides the MERGE statement that allows you 
+--to perform three actions at the same time. The following shows the
+--syntax of the MERGE statement:
+
+--MERGE target_table USING source_table
+--ON merge_condition
+--WHEN MATCHED
+--    THEN update_statement
+--WHEN NOT MATCHED
+--    THEN insert_statement
+--WHEN NOT MATCHED BY SOURCE
+--    THEN DELETE;
+
+--SQL Server MERGE statement example
+CREATE TABLE sales.category (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL,
+    amount DECIMAL(10 , 2 )
+);
+
+INSERT INTO sales.category(category_id, category_name, amount)
+VALUES(1,'Children Bicycles',15000),
+    (2,'Comfort Bicycles',25000),
+    (3,'Cruisers Bicycles',13000),
+    (4,'Cyclocross Bicycles',10000);
+
+
+CREATE TABLE sales.category_staging (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL,
+    amount DECIMAL(10 , 2 )
+);
+
+
+INSERT INTO sales.category_staging(category_id, category_name, amount)
+VALUES(1,'Children Bicycles',15000),
+    (3,'Cruisers Bicycles',13000),
+    (4,'Cyclocross Bicycles',20000),
+    (5,'Electric Bikes',10000),
+    (6,'Mountain Bikes',10000);
+
+select * from sales.category
+select * from sales.category_staging
+
+MERGE sales.category t 
+    USING sales.category_staging s
+ON (s.category_id = t.category_id)
+--MERGE specifies the target table (sales.category) that will be modified.
+--USING specifies the source table (sales.category_staging) that provides the data for comparison.
+--The ON clause defines the condition used to match rows between the target and source tables. 
+--Here, rows are matched if the category_id in both tables are equal.
+
+WHEN MATCHED
+    THEN UPDATE SET 
+        t.category_name = s.category_name,
+        t.amount = s.amount
+--If a row in the target table has the same category_id as a row in the source table, 
+--this part of the query will update the matched rows in the target table.
+--The UPDATE statement modifies the category_name and amount in the target table to match the values from the source table.
+
+
+
+WHEN NOT MATCHED BY TARGET 
+    THEN INSERT (category_id, category_name, amount)
+         VALUES (s.category_id, s.category_name, s.amount)
+--If a row exists in the source table but not in the target table,
+--it means this is a new row that needs to be inserted into the target table.
+--The INSERT clause specifies the columns to be inserted in the target table and their corresponding values from the source table.
+
+
+WHEN NOT MATCHED BY SOURCE 
+    THEN DELETE;
+--If a row exists in the target table but not in the source table, this part of the query deletes that row from the target table. 
+--This typically means the data has been removed from the source and should no longer exist in the target table.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--SQL Server Transaction
+CREATE TABLE invoices (
+  id int IDENTITY PRIMARY KEY,
+  customer_id int NOT NULL,
+  total decimal(10, 2) NOT NULL DEFAULT 0 CHECK (total >= 0)
+);
+
+CREATE TABLE invoice_items (
+  id int,
+  invoice_id int NOT NULL,
+  item_name varchar(100) NOT NULL,
+  amount decimal(10, 2) NOT NULL CHECK (amount >= 0),
+  tax decimal(4, 2) NOT NULL CHECK (tax >= 0),
+  PRIMARY KEY (id, invoice_id),
+  FOREIGN KEY (invoice_id) REFERENCES invoices (id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+);
+Select * from invoice_items
+
+BEGIN TRANSACTION;
+
+INSERT INTO invoices (customer_id, total)
+VALUES (100, 0);
+
+INSERT INTO invoice_items (id, invoice_id, item_name, amount, tax)
+VALUES (10, 1, 'Keyboard', 70, 0.08),
+       (20, 1, 'Mouse', 50, 0.08);
+
+UPDATE invoices
+SET total = (SELECT
+  SUM(amount * (1 + tax))
+FROM invoice_items
+WHERE invoice_id = 1);
+
+COMMIT;
+
+Select * from invoice_items
+--------------------
+BEGIN TRANSACTION;
+DECLARE @invoice TABLE (
+  id int
+);
+
+DECLARE @invoice_id int;
+
+INSERT INTO invoices (customer_id, total)
+OUTPUT INSERTED.id INTO @invoice
+VALUES (100, 0);
+
+SELECT
+  @invoice_id = id
+FROM @invoice;
+INSERT INTO invoice_items (id, invoice_id, item_name, amount, tax)
+VALUES (10, @invoice_id, 'Keyboard', 70, 0.08),
+       (20, @invoice_id, 'Mouse', 50, 0.08);
+
+UPDATE invoices
+SET total = (
+    SELECT SUM(amount * (1 + tax))
+    FROM invoice_items
+    WHERE invoice_id = @invoice_id
+);
+COMMIT;
